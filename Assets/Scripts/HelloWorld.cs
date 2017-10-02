3504280422cd3d3d0519feb20396d50c1c2a6742
sivers-cse <iverss@coyote.csusb.edu>
2017-10-01 21:38:43 -0700

    Hello World
    Hello World check in

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour 
+{
+
+	void HelloHello()
+	{
+	//CSE 455 Software Engineering
+	Debug.Log("Hello World!");
+
+	}
+
+}
