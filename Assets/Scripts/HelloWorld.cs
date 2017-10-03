24a720443e8fdfb03d190671b6e9ba17afb78416
delidesa <diegoarmando.gamboa@yahoo.com>
2017-10-02 22:12:36 -0700

    First Commit

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -10,6 +10,7 @@ public class HelloWorld : MonoBehaviour
 	//CSE 455 Software Engineering
 	Debug.Log("Hello World!");
 	Debug.Log("Line added by Theodore Baca"); 
-	}
+	//Adde line
+	Debug.Log("Diego Gamboa Added a line too!");
 
 }
