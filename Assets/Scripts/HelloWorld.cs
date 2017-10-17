86660e51ae5b68eb0f8cf7da5d14ac120674bf56
sivers-cse <iverss@coyote.csusb.edu>
2017-10-16 19:21:16 -0700

    Created Menu and Credits Scenes
    Basic Menu structure implementation

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -7,12 +7,12 @@ public class HelloWorld : MonoBehaviour
 
 	void HelloHello()
 	{
-	//CSE 455 Software Engineering
-	Debug.Log("Hello World!");//Steven Ivers
-	Debug.Log("Line added by Theodore Baca"); 
-	//Added line
-	Debug.Log("Diego Gamboa Added a line too!");
-    //Added a  line
-    Debug.Log("Ruben Rodriguez Checking in ");
-
+		//CSE 455 Software Engineering
+		Debug.Log("Hello World!");//Steven Ivers
+		Debug.Log("Line added by Theodore Baca"); 
+		//Added line
+		Debug.Log("Diego Gamboa Added a line too!");
+    	//Added a  line
+    	Debug.Log("Ruben Rodriguez Checking in ");
     }
+}
